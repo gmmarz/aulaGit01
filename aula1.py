74964354e2415f63d06c7d276aa3a10604365107
gmmarz <guilhermemarzano@gmail.com>
2023-07-23 11:19:00 -0300

    Test branch 2

--- aula1.py
+++ aula1.py
@@ -25,6 +25,5 @@ print(f"Data frame final filtrado\n {df_filtrado}\n----------------")
 df_filtrado2 = df[df['Idade'] >= 30]
 
 print(f'Exemplo filtrando utiliando diretamente o dataframe\n{df_filtrado2}\n---------------')
-print('Alteracao no arquivo para git teste')
-print('Alteracao 02 para verificar o push no github')
+
 print('Criando um novo branch para testes')
